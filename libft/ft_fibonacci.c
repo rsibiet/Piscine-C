be9159358c1f9d21e9fe126dc94b56cecbaab9e0
Rémy_S <remysibiet@hotmail.fr>
2018-07-13 13:37:07 +0200

    piscine C 2015

---
+++ libft/ft_fibonacci.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rsibiet <rsibiet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/08/10 16:40:05 by rsibiet           #+#    #+#             */
+/*   Updated: 2015/08/10 16:59:38 by rsibiet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	if (index == 1)
+		return (1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
